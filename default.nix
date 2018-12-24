5b7854b1431e0852ece6e5fdd843760762e4729a
Connor Prussin <connor@prussin.net>
2018-12-24 14:46:21 -0800

    Simplify overlay generation

--- default.nix
+++ default.nix
@@ -1,19 +1,8 @@
 versions: self: super:
 
 let
-
   pkgs = import ./pkgs { callPackage = super.callPackage; };
-
   normalizeVersion = builtins.replaceStrings ["."] ["_"];
-
-  overlay = pkg:
-    let
-      version = versions."${pkg}" or null;
-    in
-      if version == null
-        then {}
-        else { "${pkg}" = pkgs."${pkg}_${normalizeVersion version}"; };
-
+  getPackage = pkg: version: pkgs."${pkg}_${normalizeVersion version}";
 in
-
-overlay "nodejs" // overlay "yarn" // overlay "purescript"
+  super.lib.mapAttrs getPackage versions
