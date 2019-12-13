b6f2fd9a621a379469b65f24e9e0ed3527a8e421
Connor Prussin <connor@prussin.net>
2019-12-12 17:14:38 -0800

    Add yarn-1.21.1

---
+++ pkgs/yarn/yarn_1_21_1/default.nix
@@ -0,0 +1,27 @@
+{ stdenv, nodejs, fetchzip }:
+
+stdenv.mkDerivation rec {
+  name = "yarn-${version}";
+  version = "1.21.1";
+
+  src = fetchTarball {
+    url = "https://github.com/yarnpkg/yarn/releases/download/v${version}/yarn-v${version}.tar.gz";
+    sha256 = "1yw3v62a6309f9hr189870i9jw2a15pkians1nnfjqczzh7r5pih";
+  };
+
+  buildInputs = [ nodejs ];
+
+  installPhase = ''
+    mkdir -p $out/{bin,libexec/yarn/}
+    cp -R . $out/libexec/yarn
+    ln -s $out/libexec/yarn/bin/yarn.js $out/bin/yarn
+    ln -s $out/libexec/yarn/bin/yarn.js $out/bin/yarnpkg
+  '';
+
+  meta = with stdenv.lib; {
+    homepage = https://yarnpkg.com/;
+    description = "Fast, reliable, and secure dependency management for javascript";
+    license = licenses.bsd2;
+    maintainers = [ maintainers.offline ];
+  };
+}
