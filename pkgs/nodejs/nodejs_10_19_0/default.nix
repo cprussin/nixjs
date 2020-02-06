28ffb53e2f46fa841895c024d57e1bf1ecb3b45c
Connor Prussin <connor@prussin.net>
2020-02-06 15:47:59 -0800

    Add nodejs 10.19.0

---
+++ pkgs/nodejs/nodejs_10_19_0/default.nix
@@ -0,0 +1,9 @@
+{ stdenv, callPackage, lib }:
+
+let
+  buildNodejs = callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> {};
+in
+buildNodejs {
+  version = "10.19.0";
+  sha256 = "0sginvcsf7lrlzsnpahj4bj1f673wfvby8kaxgvzlrbb7sy229v2";
+}
