10ec226329472ea07ff602bb884dfa122662f47d
Connor Prussin <connor@prussin.net>
2020-06-11 23:42:21 -0700

    Improve github action setup:
    - Build more versions of nodejs & yarn using a build matrix
    - Rebuild every hour instead of every 6 (since we re-use the built cache, if
      there's nothing to build this should be a fairly quick no-op)
    - Clean up release.nix

--- release.nix
+++ release.nix
@@ -1,39 +1,11 @@
+versions@{
+  nodejs ? null,
+  yarn ? null
+}:
+
 let
-  nixjs = import (./.);
-  pkgsBase = import <nixpkgs> { overlays = []; };
-
-  pkgVersions = collection:
-    let
-      chompPackageName =
-        builtins.replaceStrings ["${collection}_"] [""];
-      collectionDir = (toString ./pkgs + "/${collection}");
-    in
-      map chompPackageName (builtins.attrNames (
-        builtins.readDir collectionDir
-      ));
-
-  #allVersions =
-  #  let
-  #    attrize = (nodejs: yarn: { inherit nodejs yarn; });
-  #  in
-  #    pkgsBase.lib.crossLists attrize
-  #      [
-  #        (pkgVersions "nodejs")
-  #        (pkgVersions "yarn")
-  #      ];
-
-  allVersions = [
-    {nodejs = "12_16_2"; yarn = "1_22_4"; }
-  ];
-
-  pkgs = versions: (import <nixpkgs> {
-    overlays = [ (nixjs versions) ];
-  });
+  nixjs = import (./.) versions;
+  pkgs = import <nixpkgs> { overlays = [ nixjs ]; };
 in
 
-map (versions:
-  let
-    pkgs' = pkgs versions;
-  in
-    [ pkgs'.nodejs pkgs'.yarn ]
-) allVersions
+map (pkg: pkgs."${pkg}") (builtins.attrNames versions)
